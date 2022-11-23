b927c753fbb03b69d9aec24daf4bc799c1e7d234
Tabilin <michael.tabilin@dxc.com>
2022-11-23 16:33:04 +0800

    Fix navbar in mobile view

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".nav_icon");
 const navMenu = document.querySelector(".nav_menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+  hamburger.classList.toggle("nav_icon_active");
+  navMenu.classList.toggle("nav_menu_active");
+  body.classList.toggle("bodyScrollDisable");
 });
